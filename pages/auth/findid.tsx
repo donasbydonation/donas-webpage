2d428c9fc2b593b0fcf788fdae8b89a8a2788f52
songyoungwoon <thdduddns98@naver.com>
2023-01-29 22:22:06 +0900

    Add : 회원가입 페이지 레이아웃

--- pages/auth/findid.tsx
+++ pages/auth/findid.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function FindID() {
     return (
       <div>
         create-next-app
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,7 +1,55 @@
-export default function Home() {
+import { useState } from 'react';
+import styles from '@/styles/signup.module.css'
+
+function DuplicationText(){
+  const [duplication, setDuplication] = useState(true)
+  if (duplication === false){
+    return
+  }
+  else {
+    return (
+      <span>사용 가능한 아이디입니다.</span>
+    );
+  }
+}
+
+export default function Signup() {
     return (
       <div>
-        create-next-app
+          <div className={styles.box}>
+                <div style={{display:"flex"}}>
+                    <div className={styles.sidebox_pink}></div>
+                    <span className={styles.span}>
+                        회원가입
+                    </span>
+                </div>
+
+                <div style={{display:"flex"}}>
+                  <span className={styles.subtext}>아이디</span>
+                  <div>
+                      <div><input type="text" /></div>
+                      <div><DuplicationText /></div>
+                  </div>
+                  <button>중복확인</button>
+                </div>
+
+                <div style={{display:"flex"}}>
+                  <span className={styles.subtext}>비밀번호</span>
+                  <input type="text" />
+                </div>
+
+                <div style={{display:"flex"}}>
+                  <span className={styles.subtext}>비밀번호 확인</span>
+                  <input type="text" />
+                </div>
+
+                <div style={{display:"flex"}}>
+                  <span className={styles.subtext}>이메일</span>
+                  <input type="email" />
+                </div>
+
+                <button>회원가입</button>
+          </div>
       </div>
     );
   }
