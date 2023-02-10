346155018fd5895884e00b9b205d897d9154a121
@haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-10 22:35:30 +0900

    feat: Dockerfile (#3)
    * fix: use next.js official Dockerfile
    
    * feat: add favicon.ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
