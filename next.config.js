ea553064cc7048c9d22ce134dbbe7026867ba1cb
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-10 21:59:44 +0900

    fix: use next.js official Dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    reactStrictMode: true,
+    output: 'standalone',
 }
-
-module.exports = nextConfig
