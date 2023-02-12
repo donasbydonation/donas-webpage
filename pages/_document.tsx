a79953ced78b9da80b6cd6dd7013325f915f355b
@haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-12 21:54:12 +0900

    hotfix: 405 error (#4)
    * fix: remove hostname from client-side API call
    
    * tmp: gh-action target branch to current
    
    * fix: prevent HTMLFormTag's default event to not to send POST request
    
    * fix: update website metadata
    
    * revert: target branch

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="description" content="세계 최초의 수익 공유형 SNS" />
+        <title>도나스 | Donas</title>
       </Head>
       <body>
         <Main />
