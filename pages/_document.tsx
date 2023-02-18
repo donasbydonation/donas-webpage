92d1a60facb454b373b1a69d137b78827fde07f6
songyoungwoon <34434116+songyoungwoon@users.noreply.github.com>
2023-02-19 07:48:40 +0900

    렌딩페이지 GA 세팅 오류 해결 (#9)
    * Style : 랜딩페이지 이미지 수정
    
    * Fix : GA script 코드 수정 & gtag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import * as gtag from '../lib/gtag'
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-FK2F5BW3NX"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-FK2F5BW3NX');
-        </script>
-        <!-- Google tag (gtag.js) -->
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
 
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
