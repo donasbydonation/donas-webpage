78be37e48c99411328eaad8133080f1b949f706d
@haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-19 14:54:30 +0900

    fix: add default value for NEXT_PUBLIC_GA_ID (#10)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,32 @@
 import type { AppProps } from 'next/app'
-import Top from "../src/components/Top"
-import Footer from "../src/components/Footer"
+import * as gtag from '../lib/gtag'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div style={{padding:"0px", margin:"0px"}}>      
+    <>
+      <Head>
+        {/* Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FK2F5BW3NX" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        {/* Donas metadata */}
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="description" content="세계 최초의 수익 공유형 SNS" />
+        <title>도나스 | Donas</title>
+      </Head>
       <Component {...pageProps}/>
-    </div>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import * as gtag from '../lib/gtag'
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FK2F5BW3NX"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <meta name="description" content="세계 최초의 수익 공유형 SNS" />
-        <title>도나스 | Donas</title>
-      </Head>
+      <Head />
       <body style={{margin:"4px"}}>
         <Main />
         <NextScript />
