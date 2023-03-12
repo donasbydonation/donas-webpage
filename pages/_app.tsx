81e2a5083537d90865e87c99d07312bfa80dbbb9
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-12 20:54:37 +0900

    refactor: move head contents to the _document.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,13 @@
 import type { AppProps } from 'next/app'
-import * as gtag from '../lib/gtag'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FK2F5BW3NX" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-        {/* Donas metadata */}
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <meta name="facebook-domain-verification" content="cz09jdmd69u1rmne7jwmlcfyajovf9" />
-        <meta name="description" content="세계 최초의 수익 공유형 SNS" />
+        {/* Append to `_document.tsx`'s head tag */}
+        {/* Title tag must be placed in _app.tsx */}
+        {/* @see https://nextjs.org/docs/messages/no-title-in-document-head */}
         <title>도나스 | Donas</title>
       </Head>
       <Component {...pageProps}/>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,35 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import * as gtag from '../lib/gtag'
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
-      <body style={{margin:"4px"}}>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        {/* Facebook domain verify */}
+        <meta name="facebook-domain-verification" content="cz09jdmd69u1rmne7jwmlcfyajovf9" />
+        {/* Donas metadata */}
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="description" content="세계 최초의 수익 공유형 SNS" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
