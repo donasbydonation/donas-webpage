5fe8f0e043922fde96046e7a31310ca1ac7ad45c
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-16 22:47:30 +0900

    feat: add all types of icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,9 +24,15 @@ export default function Document() {
         />
         {/* Facebook domain verify */}
         <meta name="facebook-domain-verification" content="cz09jdmd69u1rmne7jwmlcfyajovf9" />
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
         {/* Donas metadata */}
-        <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
         <meta name="description" content="세계 최초의 수익 공유형 SNS" />
       </Head>
       <body>
