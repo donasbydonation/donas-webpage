ad03e31fa8af3f8a4bdacf76dcb554307e9556f2
haeramkeem <ewqdsacxz2345@gmail.com>
2023-05-03 23:51:07 +0900

    feat: header-logo and navigation

--- ui/global-nav.tsx
+++ ui/global-nav.tsx
@@ -5,59 +5,45 @@ import Header from '@/ui/header'
 const StyledNav = styled.nav`
     position: fixed;
     display: flex;
+    align-items: center;
     justify-content: space-between;
-    background: #F9F9F9;
+    background: #FFFFFF;
     width: 100%;
-    height: 60px;
-    padding: 0 10%;
-`
+    height: 47px;
+    padding: 0px 16.46px;
+`;
 
 const StyledLink = styled(Link)`
     display: inline-flex;
     align-items: center;
     justify-content: center;
-    width: 100px;
-    height: 100%;
     cursor: pointer;
-    font-size: 18px;
+    padding: 5px;
+    font-size: 15.3px;
     font-weight: bold;
-    color: black;
+    color: #4D4D4D;
     text-decoration: none;
     &:hover{ background: #EEEEEE; }
-`
+`;
+
+const LinkContainer = styled.div`
+    display: inline-flex;
+    justify-content: space-between;
+    width: 143.22px;
+`;
 
 export default function GlobalNav(){
     return (
         <StyledNav>
             <Header />
-            <div>
+            <LinkContainer>
                 <StyledLink href="https://soulcalmfunny.notion.site/0265c96352964f298b9e600bcbbd8e9b">
                     공지사항
                 </StyledLink>
                 <StyledLink href="https://soulcalmfunny.notion.site/6c9c74f386e54f42b5763d4b74b672ce">
                     고객센터
                 </StyledLink>
-                {/* TODO: Refactoring required - Link element below is moved from `/src/components/Top.tsx` */}
-                {/*
-                <Link href="/auth/login">
-                    <button style={{
-                                    cursor:"pointer",
-                                    width:"110px",
-                                    height:"45px",
-                                    textAlign:"center",
-                                    background:"white",
-                                    borderRadius:"30px",
-                                    fontSize:"18px",
-                                    fontWeight:"bold",
-                                    backgroundColor:"#fffff",
-                                    border: "2px solid #ff3363",
-                                    color:"#ff3363"
-                                    }}>
-                            로그인
-                    </button>
-                </Link>
-                */}
-            </div>
+            </LinkContainer>
         </StyledNav>
     )
 }
