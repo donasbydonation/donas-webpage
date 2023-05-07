cd8e21d3d186413b81b91304d5420e9c092b3a2c
haeramkeem <ewqdsacxz2345@gmail.com>
2023-05-08 00:26:08 +0900

    feat: extended afreeca page

---
+++ pages/afreeca/index.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import HomeLayout from '@/ui/page-directory/HomeLayout';
+import ExtendedPlatformSchedule from '@/ui/ExtendedPlatformSchedule';
+import { axios } from '@/lib/axios';
+import { CreatorInfo } from '@/pages/api/v1/creator-infos/list';
+import { PlatformSchedule as PlatformScheduleType } from '@/pages/api/v1/schedules/list';
+import { GetServerSideProps } from 'next';
+
+type AfreecaPageProps = {
+    creatorInfos: CreatorInfo[],
+    schedules: PlatformScheduleType,
+};
+
+export default function AfreecaPage(props: AfreecaPageProps) {
+    return (
+        <HomeLayout asideCreatorInfos={props.creatorInfos} >
+            <ExtendedPlatformSchedule platform="afreeca" schedule={props.schedules} />
+        </HomeLayout>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async (ctx) => {
+    const baseUrl = "http://localhost:3000"
+    const creatorInfos = await axios.get(`${baseUrl}/api/v1/creator-infos/list`);
+
+    const commonScheduleQueryParam = [
+        `time=${encodeURIComponent(new Date().toISOString())}`,
+        `day=${ctx.query?.offset || "0"}`,
+        `provider=AFREECA`,
+    ];
+
+    // Preflight: get recommendPage
+    const preflightQueryParam = [
+        ...commonScheduleQueryParam,
+        `size=10`,
+    ];
+    const preflight = await axios.get(`${baseUrl}/api/v1/schedules/list?${preflightQueryParam.join('&')}`);
+
+    // Afreeca schedules
+    const queryParam = [
+        ...commonScheduleQueryParam,
+        `size=${preflight.data.afreeca.totalPage * 10}`,
+    ];
+    const schedules = await axios.get(`${baseUrl}/api/v1/schedules/list?${queryParam.join('&')}`);
+
+    return {
+        props: {
+            creatorInfos: creatorInfos.data,
+            schedules: schedules.data.afreeca,
+        },
+    };
+}
---
+++ ui/ExtendedPlatformSchedule.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { PlatformSchedule as PlatformScheduleType } from '@/pages/api/v1/schedules/list';
+import ScheduleCard from './ScheduleCard';
+
+const Container = styled.div`
+    margin: 20px 0px 0px 0px;
+`;
+
+const Image = styled.img`
+    height: 18px;
+    margin: 9px 0px;
+`;
+
+const List = styled.div`
+    display: flex;
+`;
+
+export default function ExtendedPlatformSchedule(props: { platform: string, schedule: PlatformScheduleType }) {
+    return (
+        <Container>
+            <Image src={`/images/icons/platforms/${props.platform}-full.svg`} alt={`${props.platform} icon`} />
+            <List>
+                {props.schedule.schedules.map((schedule, idx) => (
+                    <ScheduleCard schedule={schedule} key={idx} />
+                ))}
+            </List>
+        </Container>
+    );
+}
