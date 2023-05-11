6f120714fe6292990efaf69b49e62f4d7716bc05
haeramkeem <ewqdsacxz2345@gmail.com>
2023-05-11 22:45:44 +0900

    refactor: integrate ScheduleList component

--- ui/ExtendedPlatformSchedule.tsx
+++ ui/ExtendedPlatformSchedule.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { PlatformSchedule as PlatformScheduleType } from '@/pages/api/v1/schedules/list';
-import ScheduleCard from './ScheduleCard';
-import NoData from './NoData';
+import ScheduleList from './ScheduleList';
 
 const Container = styled.div`
     margin: 20px 0px 0px 0px;
@@ -12,25 +11,12 @@ const Image = styled.img`
     margin: 9px 0px;
 `;
 
-const List = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    min-height: 183px;
-    align-items: center;
-`;
-
 export default function ExtendedPlatformSchedule(props: { platform: string, schedule: PlatformScheduleType }) {
     const schedules = props.schedule.schedules.filter(s => (s.broadcastLink !== ""));
     return (
         <Container>
             <Image src={`/images/icons/platforms/${props.platform}-full.svg`} alt={`${props.platform} icon`} />
-            <List>
-                {
-                    (schedules.length !== 0)
-                    ? schedules.map((schedule, idx) => (<ScheduleCard schedule={schedule} key={idx} />))
-                    : <NoData>일정이 없습니다.</NoData>
-                }
-            </List>
+            <ScheduleList schedule={props.schedule} />
         </Container>
     );
 }
--- ui/PlatformSchedule.tsx
+++ ui/PlatformSchedule.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 import { PlatformSchedule as PlatformScheduleType } from '@/pages/api/v1/schedules/list';
-import ScheduleCard from './ScheduleCard';
 import PlatformSchedulePagination from './PlatformSchedulePagination';
 import ExtendPlatformButton from './ExtendPlatformButton';
-import NoData from './NoData';
+import ScheduleList from './ScheduleList';
 
 const Container = styled.div`
     margin: 20px 0px 0px 0px;
@@ -12,14 +11,6 @@ const Container = styled.div`
 const Image = styled.img`
     height: 18px;
     margin: 9px 0px;
-`;
-
-const List = styled.div`
-    display: flex;
-    min-height: 183px;
-    align-items: center;
-    flex-wrap: wrap;
-    overflow: hidden;
 `;
 
 const Footer = styled.div`
@@ -32,17 +23,10 @@ const Footer = styled.div`
 export type PlatformScheduleProps = {platform: string, schedule: PlatformScheduleType, selectedPage: number};
 
 export default function PlatformSchedule(props: PlatformScheduleProps) {
-    const schedules = props.schedule.schedules.filter(s => (s.broadcastLink !== ""));
     return (
         <Container>
             <Image src={`/images/icons/platforms/${props.platform}-full.svg`} alt={`${props.platform} icon`} />
-            <List>
-                {
-                    (schedules.length !== 0)
-                    ? schedules.map((schedule, idx) => (<ScheduleCard schedule={schedule} key={idx} />))
-                    : <NoData>일정이 없습니다.</NoData>
-                }
-            </List>
+            <ScheduleList schedule={props.schedule} />
             <Footer>
                 <span>{" "}</span>
                 <PlatformSchedulePagination
