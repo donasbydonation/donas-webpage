bad1d53e81c9566349d380f17673a13d41a6fef5
@haeramkeem <haeram.kim1@gmail.com>
2023-05-12 00:22:28 +0900

    [Home] feat: platform change extend button (#26)

--- ui/header.tsx
+++ ui/header.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components'
+import Link from 'next/link';
 
 const LogoImg = styled.img`
     height: 20px;
     width: 122px;
-`
+`;
 
 export default function Header(){
     return (
         <header>
-            <LogoImg src= "/images/logo.svg" alt="Donas logo" />
+            <Link href="/">
+                <LogoImg src= "/images/logo.svg" alt="Donas logo" />
+            </Link>
         </header>
     )
 }
