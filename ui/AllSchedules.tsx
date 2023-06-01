75872687e48189ff762ef13a8ca6a08e2d416fa7
haeramkeem <ewqdsacxz2345@gmail.com>
2023-06-01 23:57:59 +0900

    fix: rename file and adjust to v2 api and layout

---
+++ ui/AllSchedules.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { Schedule } from '@/pages/api/v2/schedules';
+import AllSchedulePagination from './AllSchedulePagination';
+import ScheduleList from './ScheduleList';
+import ScheduleHeader from './ScheduleHeader';
+
+const Container = styled.div`
+    margin: 20px 0px 0px 0px;
+`;
+
+const Footer = styled.div`
+    display: flex;
+    padding: 3px 0px 16px 0px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export type AllSchedulesProps = {totalPage: number, selectedPage: number, schedules: Schedule[]};
+
+export default function AllSchedules(props: AllSchedulesProps) {
+    return (
+        <Container>
+            <ScheduleHeader platform={["afreeca", "twitch", "youtube"]} />
+            <ScheduleList schedules={props.schedules} />
+            <Footer>
+                <AllSchedulePagination
+                    eventKey={`scheduleIdx`}
+                    totalPage={props.totalPage}
+                    selectedPage={props.selectedPage}
+                />
+            </Footer>
+        </Container>
+    );
+}
