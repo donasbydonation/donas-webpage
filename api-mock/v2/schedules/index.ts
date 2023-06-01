51278a53d92326d858601cc51813cc5681e94c50
haeramkeem <ewqdsacxz2345@gmail.com>
2023-06-01 23:56:31 +0900

    fix: modify response dto for v2 api

---
+++ api-mock/v2/schedules/index.ts
@@ -0,0 +1,45 @@
+import type { ResponseBodyDTO } from '@/pages/api/v2/schedules';
+
+export const data: ResponseBodyDTO = {
+    "totalPage": 2,
+    "recommendPage": 2,
+    "nowPage": 1,
+    "schedules": [
+        {
+            "creatorName": "creator-name15",
+            "profileImage": "https://yt3.ggpht.com/bHK1E0WBTePF6n5EgRTnEIcmqbnHeuJ8_2NZz5WwB5ggOZlAzWHCwK7_6rlcJeqp0Lci4bHq7w=s176-c-k-c0x00ffffff-no-rj-mo",
+            "platforms": [
+                {
+                    "provider": "YOUTUBE",
+                    "broadcastLink": "broadcastLink15"
+                },
+                {
+                    "provider": "AFREECA",
+                    "broadcastLink": "broadcastLink15"
+                },
+                {
+                    "provider": "TWITCH",
+                    "broadcastLink": "broadcastLink15"
+                }
+            ],
+            "title": "title15",
+            "bannerImage": "bannerImage15",
+            "description": "description15",
+            "scheduledTime": "2023-05-03T15:00:00Z"
+        },
+        {
+            "creatorName": "creator-name16",
+            "profileImage": "https://yt3.ggpht.com/bHK1E0WBTePF6n5EgRTnEIcmqbnHeuJ8_2NZz5WwB5ggOZlAzWHCwK7_6rlcJeqp0Lci4bHq7w=s176-c-k-c0x00ffffff-no-rj-mo",
+            "platforms": [
+                {
+                    "provider": "TWITCH",
+                    "broadcastLink": "broadcastLink16"
+                }
+            ],
+            "title": "title16",
+            "bannerImage": "bannerImage16",
+            "description": "description16",
+            "scheduledTime": "2023-05-03T16:00:00Z"
+        },
+    ]
+};
