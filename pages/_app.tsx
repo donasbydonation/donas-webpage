30a6be6b27470cb7818dda10ad7b7e1f1e0e70e0
songyoungwoon <thdduddns98@naver.com>
2023-06-29 22:18:57 +0900

    Add : 애드센스 코드 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         {/* Title tag must be placed in _app.tsx */}
         {/* @see https://nextjs.org/docs/messages/no-title-in-document-head */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6768978112594947"
+          crossorigin="anonymous"></script>
         <title>도나스 | Donas</title>
       </Head>
       <GlobalNav />
